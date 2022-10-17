786d2bf880dfb1f81676d664d3552b35fd89164c
NafisaMaliyat-iut <nafisamaliyat@iut-dhaka.edu>
2022-10-18 02:17:48 +0600

    landing page layout complete
    -added course list
    -added about us feature
    -added href to sections of page
    note: motivation portion to be completed later on

---
+++ code/frontend/landingPage/landingPage.js
@@ -0,0 +1,20 @@
+
+
+var swiper = new Swiper(".course-list", {
+    slidesPerView: 4,
+    spaceBetween: 2,
+    direction: 'horizontal',
+    loop: true,
+
+
+    centerInsufficientSlides:true,
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+});
+
+
