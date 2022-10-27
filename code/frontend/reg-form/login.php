04eeec69a7b6624e7381948010d1177c8310644b
Shanta Maria <shantamaria@iut-dhaka.edu>
2022-10-27 12:03:18 +0600

    edited page redirection for login
    successful = user-profile
    unsuccessful = same page

--- code/frontend/reg-form/login.php
+++ code/frontend/reg-form/login.php
@@ -1,43 +1,44 @@
 /*
-    //Title: User Info Authentication while Log In
-    //Author: Shanta Maria
-    //Date: 18/10/2022
+//Title: User Info Authentication while Log In
+//Author: Shanta Maria
+//Date: 18/10/2022
 */
 
-<?php      
-    require "connection.php"; 
+<?php
+require "connection.php";
 
-    $email = $_POST['email'];  
-    $password = $_POST['spassword'];  
-      
-        //to prevent from mysqli injection  
-        $email = stripcslashes($email);  
-        $password = stripcslashes($password);  
-        $email = mysqli_real_escape_string($con, $email);  
-        $password = mysqli_real_escape_string($con, $password);  
-      
-        $sql = "SELECT * FROM students where Email = '$email'"; 
-        $result = mysqli_query($con, $sql);  
-        $row = mysqli_fetch_array($result, MYSQLI_ASSOC);  
-        $count = mysqli_num_rows($result);
+$email = $_POST['email'];
+$password = $_POST['spassword'];
 
-        $password_query = "SELECT Password FROM students where Email = '$email'";
-        $result2 = mysqli_query($con, $password_query);
-        $row2 = mysqli_fetch_assoc($result2);
-        $hashed_password= $row["Password"];
-        $pass_check=password_verify($password, $hashed_password);  
-          
-        if($count == 1 && $pass_check){
+//to prevent from mysqli injection  
+$email = stripcslashes($email);
+$password = stripcslashes($password);
+$email = mysqli_real_escape_string($con, $email);
+$password = mysqli_real_escape_string($con, $password);
 
-            echo "Login successful";  
-        }  
-        else{  
-            echo "Login failed. Invalid username or password.";  
-        }
+$sql = "SELECT * FROM students where Email = '$email'";
+$result = mysqli_query($con, $sql);
+$row = mysqli_fetch_array($result, MYSQLI_ASSOC);
+$count = mysqli_num_rows($result);
 
-        mysqli_free_result($result);
-        mysqli_free_result($result2);
+$password_query = "SELECT Password FROM students where Email = '$email'";
+$result2 = mysqli_query($con, $password_query);
+$row2 = mysqli_fetch_assoc($result2);
+$hashed_password = $row["Password"];
+$pass_check = password_verify($password, $hashed_password);
 
-        mysqli_close($con);
-        
+if ($count == 1 && $pass_check) {
+
+    header('Location: ../user-profile/user-profile.html');
+    exit();
+} else {
+    header('Location: reg-form.html#login');
+    exit();
+}
+
+mysqli_free_result($result);
+mysqli_free_result($result2);
+
+mysqli_close($con);
+
 ?>
