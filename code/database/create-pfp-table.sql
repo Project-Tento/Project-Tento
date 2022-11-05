10c83fbd6d63893f657f3bac7a5db6d4a607402b
Shanta Maria <shantamaria@iut-dhaka.edu>
2022-11-05 16:20:49 +0600

    moved directory from backend to database

---
+++ code/database/create-pfp-table.sql
@@ -0,0 +1,12 @@
+/*
+    //Title: Creating pfp table
+    //Author: Shanta Maria
+    //Date: 28/10/2022
+*/
+
+---creating table for profile pictures---
+CREATE OR REPLACE TABLE pfp 
+(
+    pfp_id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    pfp_link TEXT
+);
