eb039182796cf7ce82f5296924648ce51def91bd
Shanta Maria <shantamaria@iut-dhaka.edu>
2022-11-05 17:11:44 +0600

    edited pfp table sql file
    specified db name when creating table

--- code/database/create-pfp-table.sql
+++ code/database/create-pfp-table.sql
@@ -5,7 +5,7 @@
 */
 
 ---creating table for profile pictures---
-CREATE OR REPLACE TABLE pfp 
+CREATE OR REPLACE TABLE tentodatabase.pfp 
 (
     pfp_id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
     pfp_link TEXT
