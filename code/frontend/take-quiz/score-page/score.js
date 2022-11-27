b6b1f16aad9f95f0902193a7634774f840a2a680
NafisaMaliyat-iut <nafisamaliyat@iut-dhaka.edu>
2022-11-27 11:39:01 +0600

    added score pag
    note: fix confetti (only when score is above average)

---
+++ code/frontend/take-quiz/score-page/score.js
@@ -0,0 +1,10 @@
+const jsConfetti = new JSConfetti();
+
+( jsConfetti.addConfetti({
+    confettiColors: [
+        '#E6ECEF', '#181887', '#0FD7DF', '#fff', '#A6B2FC', '#9EF0FF' , '#716AFF', '#fdff6a', '#ff718d',
+      ],
+      emojiSize: 40,
+      confettiNumber: 500,
+    })
+  );
