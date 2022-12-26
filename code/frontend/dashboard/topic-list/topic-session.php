1fb778d603aede8a2d5d7675f1bed7f93d36edc7
Shanta Maria <shantamaria@iut-dhaka.edu>
2022-12-27 01:33:00 +0600

    added topic ID to session variables

--- code/frontend/dashboard/topic-list/topic-session.php
+++ code/frontend/dashboard/topic-list/topic-session.php
@@ -7,6 +7,9 @@ include "connection.php";
 if (isset($_POST['theTopicName'])) {
 
     $_SESSION['userTopicChoice'] = $_POST['theTopicName'];
+    $_SESSION['setTopicID'] = $_POST['theTopicID'];
+
+    //echo $_SESSION['setTopicID'];
 
     header('Location: ../../dynamic-mcq/take-quiz/take-quiz.php');
 }
