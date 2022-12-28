ac6923e18e8f844c3d33903d92c18d837fc4ad37
Shanta Maria <shantamaria@iut-dhaka.edu>
2022-12-28 23:15:09 +0600

    implemented reset password

---
+++ code/frontend/dashboard/settings/reset-pass.php
@@ -0,0 +1,50 @@
+<?php
+
+include "../profile/connection.php";
+
+session_start();
+
+function isPasswordValid($password) {
+    return (!preg_match("/^(?=.*[!@#$%^&*-])(?=.*[0-9])(?=.*[A-Z]).{8,20}$/",$password)) ? FALSE : TRUE;
+}
+
+if(isset($_POST['reset-pass']))
+{
+    $currPass = mysqli_real_escape_string($conn, $_POST['updateCurrentPass']);
+    $newPass = mysqli_real_escape_string($conn, $_POST['updateNewPass']);
+    $newPassCon = mysqli_real_escape_string($conn, $_POST['updateConfirmPass']);
+    $hashedPassword = password_hash($newPass, PASSWORD_BCRYPT); //bcrypt algorithm used
+
+    $userID = $_SESSION['user_id'];
+    $sqlForPass = "SELECT Password FROM students WHERE UserID = '$userID'";
+    $resultPass = $conn->query($sqlForPass);
+    $row = $resultPass->fetch_assoc();
+
+    $passCheck = password_verify($currPass, $row['Password']);
+
+    if($currPass == $row['Password'])
+    {
+        header('Location: settings.php');
+    } elseif (!isPasswordValid($newPass)) {
+
+        header('Location: settings.php');
+    } elseif ($newPass != $newPassCon) {
+
+        header('Location: settings.php');
+    }  else {
+
+        $update = "UPDATE students SET Password = '$hashedPassword' WHERE UserID = '$userID'";
+        $result = $conn->query($update);
+
+        if($result)
+        {
+            header('Location: ../user-dashboard.php');
+        }
+        else
+        {
+            header('Location: settings.php');
+        }
+    }
+}
+
+?>
