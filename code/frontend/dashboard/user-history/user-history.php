49307f23a9c3f27fc8776ca10dad5dea7177e925
NafisaMaliyat-iut <nafisamaliyat@iut-dhaka.edu>
2022-12-29 04:20:01 +0600

    fixed sidebar of history page

--- code/frontend/dashboard/user-history/user-history.php
+++ code/frontend/dashboard/user-history/user-history.php
@@ -52,7 +52,7 @@ if (!isset($_SESSION["user_id"])) {
         </div>-->
 
             <div class="p-4">
-                <h1><img class="logoImg" src="../favicon.png"><a href="../user-dashboard.php" class="logo"> TENTO</a>
+                <h1><img class="logoImg" src="../favicon.png"><a href="../user-dashboard.php" class="logo logoHistory"> TENTO</a>
                 </h1>
                 <hr>
                 <ul class="list-unstyled components mb-5">
