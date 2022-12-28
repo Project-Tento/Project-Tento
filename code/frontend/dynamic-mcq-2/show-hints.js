3eb9811004df56eb1a91bc81c39d3f7c297a6b30
Shanta Maria <shantamaria@iut-dhaka.edu>
2022-12-29 03:35:41 +0600

    shifted content from one folder to another

--- code/frontend/dynamic-mcq-2/show-hints.js
+++
@@ -1,7 +0,0 @@
-function showHint(x){
-    const hints = document.querySelectorAll('.hint-text');
-    hints[x-1].classList.remove('d-none');
-
-    const hintButtons = document.querySelectorAll('.hintButtons');
-    hintButtons[x-1].classList.add('.darkerButton');
-}
