4c36a720c77bf5be1c8a0a002d2920dc747ce0db
Shanta Maria <shantamaria@iut-dhaka.edu>
2022-12-28 19:03:29 +0600

    modal implementation (not working)

---
+++ code/frontend/reg-form/modal.js
@@ -0,0 +1,27 @@
+$('#signup').click(function () {
+  //code for the email validation alert to appear
+  $('.emailValidationAlert').addClass("show");
+  $('.emailValidationAlert').removeClass("hide");
+  $('.emailValidationAlert').addClass("showAlert");
+  var correctMsg = document.getElementsByClassName("correctMsg");
+  correctMsg.innerHTML = x;
+
+
+  //code for the wrong pattern alert to appear
+  $('.wrongPatternAlert').addClass("show");
+  $('.wrongPatternAlert').removeClass("hide");
+  $('.wrongPatternAlert').addClass("showAlert");
+});
+
+$('.email-validation-alert-close-btn').click(function () {
+  $('.emailValidationAlert').removeClass("show");
+  $('.emailValidationAlert').addClass("hide");
+  $('.emailValidationAlert').removeClass("showAlert");
+});
+
+
+$('.wrong-pattern-close-btn').click(function () {
+  $('.wrongPatternAlert').removeClass("show");
+  $('.wrongPatternAlert').addClass("hide");
+  $('.wrongPatternAlert').removeClass("showAlert");
+});
--- code/frontend/reg-form/register-form.php
+++ code/frontend/reg-form/register-form.php
@@ -1,4 +1,3 @@
-
 <?php
 
 include "backend/reg-ver.php";
@@ -20,11 +19,13 @@ include "backend/reg-ver.php";
     <meta charset="UTF-8">
     <title>Tento | Register</title>
     <link rel="stylesheet" href="reg-form.css">
+    <link rel="stylesheet" href="modal.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css" />
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>
     <link rel="shortcut icon" href="favicon.ico">
 
 </head>
@@ -38,6 +39,24 @@ include "backend/reg-ver.php";
             <div class="logo" onclick="window.location.href='../landingPage/landingPage.html';">
                 <img src="favicon.png"> TENTO
             </div>
+
+            <div class="alert emailValidationAlert hide">
+                <span class="fas fa-check-circle"></span>
+                <span class="msg correctMsg">Your verification mail has been sent! &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>
+                <div class="email-validation-alert-close-btn">
+                    <span class="fas fa-times"></span>
+                </div>
+            </div>
+
+            <div class="alert wrongPatternAlert hide">
+                <span class="fas fa-exclamation-circle"></span>
+                <span class="msg incorrectMsg">Invalid Iput!&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>
+                <div class="wrong-pattern-close-btn">
+                    <span class="fas fa-times"></span>
+                </div>
+            </div>
+
+
 
         </nav>
 
@@ -54,10 +73,7 @@ include "backend/reg-ver.php";
 
                         <!--<button aria-label='delete item' onClick="window.location.href='../landingPage/landingPage.html'" class='close-button'>X</button>-->
 
-                        <h3>REGISTRATION
-
-                        </h3>
-
+                        <h3>REGISTRATION</h3>
 
 
                         <form id="register-form" method="post" action="" autocomplete="off">
@@ -125,7 +141,35 @@ include "backend/reg-ver.php";
 
                             </div>
 
-                            <button class="submitButton" type="submit" name="signup">Sign Up</button>
+                            <?php
+                            //echo $_SESSION['error'];
+                            // if (str_contains($_SESSION['error'], "emailSent")) {
+                                // echo "OKK";
+                            // } else if(isset($_POST['signup'])) { ?>
+                                <!-- <script type=text/javascript>
+                                    $("#signup").click(function() {
+                                        // setTimeout(() => {
+                                        //     ;
+                                        // }, 50000);
+                                        //code for the email validation alert to appear
+                                        $(".wrongPatternAlert").addClass("show");
+                                        $(".wrongPatternAlert").removeClass("hide");
+                                        $(".wrongPatternAlert").addClass("showAlert");
+                                    });
+                                    $(".wrong-pattern-close-btn").click(function() {
+                                        $(".wrongPatternAlert").removeClass("show");
+                                        $(".wrongPatternAlert").addClass("hide");
+                                        $(".wrongPatternAlert").removeClass("showAlert");
+                                    });
+                                </script> -->
+
+                            <?php
+                            //} ?>
+
+                            <!--CHANGE BUTTON TYPE TO TYPE SUBMIT OR NOTHING FOR THE FORM TO SUBMIT-->
+                            <button type="submit" class="submitButton" name="signup" id="signup">Sign Up</button>
+
+                            
 
                             <div class="toggle-register-login">
                                 Already have an account? &nbsp;<a href="login-form.php" class="forgot" id="forgot">Sign in
@@ -133,6 +177,7 @@ include "backend/reg-ver.php";
                             </div>
                         </form>
                         <!--register form ends-->
+
 
                     </div>
                 </div>
@@ -146,14 +191,30 @@ include "backend/reg-ver.php";
         </div>
     </div>
 
-
+    <!-- <script type=text/javascript>
+        $(document).ready(function() {
+            $("#signup").click(function() {
+                //code for the email validation alert to appear
+                $(".emailValidationAlert").addClass("show");
+                $(".emailValidationAlert").removeClass("hide");
+                $(".emailValidationAlert").addClass("showAlert");
+            });
+            $(".email-validation-alert-close-btn").click(function() {
+                $(".emailValidationAlert").removeClass("show");
+                $(".emailValidationAlert").addClass("hide");
+                $(".emailValidationAlert").removeClass("showAlert");
+            });
+        })
+    </script> -->
 
 
 
     <script src="reg-form.js"></script>
+    <!-- <script src="modal.js"></script> -->
     <script src="email-validation.js"></script>
     <script src="password-validation.js"></script>
     <script src="https://code.iconify.design/iconify-icon/1.0.1/iconify-icon.min.js"></script>
+
 
 </body>
 
